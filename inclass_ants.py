0fafe08393aeeb05689b2c82c407e651d352e99a
Ming Zhong <Ming@D-10-65-71-21.dhcp4.washington.edu>
2017-04-25 19:18:36 -0700

    adding inclass_ants.py with all function skeletons

---
+++ inclass_ants.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+#=======================================================================
+#                        General Documentation
+
+"""
+   See function docstrings for description.
+"""
+
+#-----------------------------------------------------------------------
+#                       Additional Documentation
+#
+#
+#
+#=======================================================================
+
+#---------------- Module General Import and Declarations ---------------
+import random
+import math
+import matplotlib.pyplot as plt
+import Numpy as N
+
+
+
+
+def initAntGrid():
+    '''   '''
+    
+def init_pher_grid():
+    '''   '''
+    
+def diffusion():
+    '''   '''
+
+def apply_diffusion_extended():
+    '''   '''
+    
+    
+def sense():
+    '''   '''
+    
+    
+def walk():
+    '''   '''
+    
+    
+def apply_sense_extended():
+    '''   '''
+    
+    
+def visualize():
+    '''   '''
+    
+
+
+#---------------------------------------Main------------------------------------
+
+
+
+
+
+# ===== end file =====	 
